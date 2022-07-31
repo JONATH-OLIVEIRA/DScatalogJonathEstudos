9782c2044d628b0255d5a499d23e8d7da7427966
Jonath Oliveira <akiles22a@gmail.com>
2022-07-31 15:20:08 -0300

    Delete implementado

--- src/main/java/com/estudosjonath/dscatalog/services/CategoryService.java
+++ src/main/java/com/estudosjonath/dscatalog/services/CategoryService.java
@@ -5,12 +5,15 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.estudosjonath.dscatalog.dto.CategoryDTO;
 import com.estudosjonath.dscatalog.entities.Category;
 import com.estudosjonath.dscatalog.repositories.CategoryRepository;
+import com.estudosjonath.dscatalog.services.exceptions.DataBaseException;
 import com.estudosjonath.dscatalog.services.exceptions.EntityNotFoundException;
 
 @Service
@@ -53,6 +56,16 @@ public class CategoryService {
 		} catch (javax.persistence.EntityNotFoundException e) {
 			throw new EntityNotFoundException("Id not found" + id);
 
+		}
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new EntityNotFoundException("Id not found" + " " + id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataBaseException("Integreti Violation");
 		}
 	}
 
